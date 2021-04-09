32b8bfc04c2b69e8d6ac66eaa371f4f1c7c705e1
Gracy-Wen <72426744+Gracy-Wen@users.noreply.github.com>
2021-04-09 10:59:57 +0800

    added story game

---
+++ story-game/story.js
@@ -0,0 +1,150 @@
+var meow = new Audio("audio/Meowing-sound.wav")
+var meow2 = new Audio("audio/Cats-meowing.mp3")
+var meow3 = new Audio("audio/Cat-meow-sound-effect.wav")
+var meow4 = new Audio("audio/Cat-meow-sound-3.mp3")
+var meow5 = new Audio("audio/Angry-cat-sounds.mp3")
+
+
+
+
+
+function B1(){
+    document.querySelector(".P2").classList.remove("invisible")
+    document.querySelector(".T2").classList.remove("invisible")
+    document.querySelector(".B2").classList.remove("invisible")
+    document.querySelector(".B1").classList.add("invisible")
+    document.querySelector(".T1").classList.add("invisible")
+    document.querySelector(".P1").classList.add("invisible")
+    document.querySelector(".B21").classList.remove("invisible")
+    meow5.play()
+}
+function B2(){
+    document.querySelector(".P3").classList.remove("invisible")
+    document.querySelector(".T3").classList.remove("invisible")
+    document.querySelector(".B3").classList.remove("invisible")
+    document.querySelector(".B21").classList.add("invisible")
+    document.querySelector(".B2").classList.add("invisible")
+    document.querySelector(".T2").classList.add("invisible")
+    document.querySelector(".P2").classList.add("invisible")
+    meow5.play()
+}
+function B3(){
+    document.querySelector(".P3").classList.add("invisible")
+    document.querySelector(".B3").classList.add("invisible")
+    document.querySelector(".T3").classList.add("invisible")
+    document.querySelector(".P511").classList.remove("invisible")
+    document.querySelector(".P522").classList.remove("invisible")
+    document.querySelector(".T4").classList.remove("invisible")
+    document.querySelector(".B411").classList.remove("invisible")
+    document.querySelector(".B422").classList.remove("invisible")
+    meow.play()
+
+}
+function B41(){
+    document.querySelector(".B51").classList.remove("invisible")
+    document.querySelector(".B52").classList.remove("invisible")
+    document.querySelector(".T51").classList.remove("invisible")
+    document.querySelector(".P523").classList.remove("invisible")
+    document.querySelector(".P511").classList.add("invisible")
+    document.querySelector(".P522").classList.add("invisible")
+    document.querySelector(".T4").classList.add("invisible")
+    document.querySelector(".B411").classList.add("invisible")
+    document.querySelector(".B422").classList.add("invisible")
+    meow5.play()
+
+}
+
+function B42(){
+    document.querySelector(".B51").classList.remove("invisible")
+    document.querySelector(".B52").classList.remove("invisible")
+    document.querySelector(".T52").classList.remove("invisible")
+    document.querySelector(".T52").classList.remove("invisible")
+    document.querySelector(".P523").classList.remove("invisible")
+    document.querySelector(".P511").classList.add("invisible")
+    document.querySelector(".P522").classList.add("invisible")
+    document.querySelector(".T4").classList.add("invisible")
+    document.querySelector(".B411").classList.add("invisible")
+    document.querySelector(".B422").classList.add("invisible")
+    meow5.play()
+
+}
+function B51(){
+    document.querySelector(".B51").classList.add("invisible")
+    document.querySelector(".B52").classList.add("invisible")
+    document.querySelector(".T52").classList.add("invisible")
+    document.querySelector(".T51").classList.add("invisible")
+    document.querySelector(".P523").classList.add("invisible")
+    document.querySelector(".B61").classList.remove("invisible")
+    document.querySelector(".P61").classList.remove("invisible")
+    document.querySelector(".T61").classList.remove("invisible")
+    meow4.play()
+}
+
+function B52(){
+    document.querySelector(".B51").classList.add("invisible")
+    document.querySelector(".B52").classList.add("invisible")
+    document.querySelector(".T52").classList.add("invisible")
+    document.querySelector(".T51").classList.add("invisible")
+    document.querySelector(".P523").classList.add("invisible")
+    document.querySelector(".B62").classList.remove("invisible")
+    document.querySelector(".P62").classList.remove("invisible")
+    document.querySelector(".T62").classList.remove("invisible")
+    meow4.play()
+}
+
+function B62 (){
+    document.querySelector(".B62").classList.add("invisible")
+    document.querySelector(".P62").classList.add("invisible")
+    document.querySelector(".T62").classList.add("invisible")
+    document.querySelector(".B7").classList.remove("invisible")
+    document.querySelector(".P7").classList.remove("invisible")
+    document.querySelector(".T7").classList.remove("invisible")
+    meow3.play()
+
+}
+
+function B61 (){
+    document.querySelector(".B61").classList.add("invisible")
+    document.querySelector(".P61").classList.add("invisible")
+    document.querySelector(".T61").classList.add("invisible")
+    document.querySelector(".B7").classList.remove("invisible")
+    document.querySelector(".P7").classList.remove("invisible")
+    document.querySelector(".T7").classList.remove("invisible")
+    meow3.play()
+}
+function B7 (){
+    document.querySelector(".B7").classList.add("invisible")
+    document.querySelector(".P7").classList.add("invisible")
+    document.querySelector(".T7").classList.add("invisible")
+    document.querySelector(".B81").classList.remove("invisible")
+    document.querySelector(".B82").classList.remove("invisible")
+    document.querySelector(".P8").classList.remove("invisible")
+    document.querySelector(".T8").classList.remove("invisible")
+    meow2.play()
+}
+
+function B81(){
+    document.querySelector(".B81").classList.add("invisible")
+    document.querySelector(".B82").classList.add("invisible")
+    document.querySelector(".P8").classList.add("invisible")
+    document.querySelector(".T8").classList.add("invisible")
+    document.querySelector(".B91").classList.remove("invisible")
+    document.querySelector(".P91").classList.remove("invisible")
+    document.querySelector(".T91").classList.remove("invisible")
+    meow4.play()
+}
+
+function B82(){
+    document.querySelector(".B81").classList.add("invisible")
+    document.querySelector(".B82").classList.add("invisible")
+    document.querySelector(".P8").classList.add("invisible")
+    document.querySelector(".T8").classList.add("invisible")
+    document.querySelector(".B92").classList.remove("invisible")
+    document.querySelector(".P92").classList.remove("invisible")
+    document.querySelector(".T92").classList.remove("invisible")
+    meow2.play()
+}
+
+
+
+
